9ca16feb64788ca66fc4e8700cb9fd5b94d8afae
sysbeetion <1049625636@qq.com>
2021-02-22 15:36:35 +0800

    基础界面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> weibo app </title>
+</head>
+
+<body>
+    <h1>帮助页</h1>
+</body>
+
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', '帮助')
+@section('content')
+    <h1>关于页面</h1>
+@stop
+
