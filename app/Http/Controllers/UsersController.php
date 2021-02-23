1a484b8ad06355dc5378655abc9fc2a01b856c18
sysbeetion <1049625636@qq.com>
2021-02-23 17:42:27 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,4 +37,27 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
         //为什么这里要用中括号？ route() 方法会自动获取 Model 的主键 redirect()->route('users.show', [$user->id]);
     }
+
+    public function edit(User $user){
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request){
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success' ,'个人资料更新成功');
+
+        return redirect()->route('users.show', $user);
+    }
+
 }
