361a6b4c228b74c3ce4de588ae3f1d3e2caa95db
sysbeetion <1049625636@qq.com>
2021-02-24 10:44:14 +0800

    查看用户列表

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+//      会发现样式错乱，这是因为默认 Laravel 分页没有使用 Bootstrap ，我们只需要在 AppServiceProvider 中设置使用 Bootstrap 即可：
+        \Illuminate\Pagination\Paginator::useBootstrap();
         Schema::defaultStringLength(191);
     }
 }
