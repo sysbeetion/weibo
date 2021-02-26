8f1db10fcc40603ef37bb1b1fc1c2dc8f4fa7f0e
sysbeetion <1049625636@qq.com>
2021-02-26 15:06:52 +0800

    微博首页

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable
     public function statuses(){
         return $this->hasMany(Status::class);//这算什么
     }
+
+    public function feed(){
+        return $this->statuses()->orderBy('created_at', 'desc');
+    }
+
 }
